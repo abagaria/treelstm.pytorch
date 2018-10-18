ddd41a711bf945c032e17e7761aebecae9ee3cd7
Akhil Bagaria <akhil_bagaria@brown.edu>
2018-10-18 15:55:05 -0400

    No need to preprocess SICK dataset anymore

--- fetch_and_preprocess.sh
+++ fetch_and_preprocess.sh
@@ -4,6 +4,6 @@ bash scripts/download.sh
 
 CLASSPATH="lib:lib/stanford-parser/stanford-parser.jar:lib/stanford-parser/stanford-parser-3.5.1-models.jar"
 javac -cp $CLASSPATH lib/*.java
-python scripts/preprocess-sick.py
+#python scripts/preprocess-sick.py
 
 mkdir -p checkpoints/
